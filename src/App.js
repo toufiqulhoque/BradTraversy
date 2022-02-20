22c0e9fc13a434fe24c01c9f7a410c5b14aefea7
toufiqulhoque <xtoufiqxx@gmail.com>
2022-02-20 11:48:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+function App(){
+const title='Blog Post'
+const body='This is my blog post'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const comments=[
+    {id:1,text:'Comment one'},
+    {id:2,text:'Comment two'},
+    {id:3,text:'Comment three'}
+]
+const loading=false
+const showComments= true
+if(loading){
+    return <h1>Loading...</h1>
 }
 
-export default App;
+    return (
+       <div className="container">
+            <h1>{title.toUpperCase}</h1>
+        <p>{body}</p>
+<br />
+        {showComments? <ul>
+          {
+             comments.map((comment,index)=>(
+                <li key={index}>{comment.text}</li>
+               
+             )
+                
+             )
+              
+          }
+        </ul>:'no'}
+        <br />
+        <br />
+        {Math.random()*5}
+
+        <h3>Comments: {comments.length}</h3>
+        
+
+
+       </div>
+    )
+}
+
+export default App
